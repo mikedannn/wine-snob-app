f1a585eb27a32d903c77c9c596f02e4015586d0d
Michael <michael.daniels1013@gmail.com>
2023-01-13 16:15:45 -0600

    adjusted forms

--- client/src/SignUpForm.js
+++ client/src/SignUpForm.js
@@ -11,31 +11,60 @@ function SignUpForm() {
     })
 
     return (
-        <div>
-          <h1>Create Account</h1>
+        <div className="signUpForm">
+          <h1 >Create Account</h1>
           <form>
             <div>
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" value={newUserInfo.name}></input>
-            </div> <br />
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        value={newUserInfo.name}>
+                    </input>
+            </div> 
+                <br />
             <div>
                 <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" value={newUserInfo.username}></input>
-            </div> <br />
+                    <input 
+                        type="text" 
+                        id="username" 
+                        name="username" 
+                        value={newUserInfo.username}>
+                    </input>
+            </div> 
+                <br />
             <div>
                 <label htmlFor="password">Password: </label>
-                <input type="password" id="password" name="password" value={newUserInfo.password} />
-            </div><br />
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        value={newUserInfo.password}>
+                    </input>
+            </div>
+                <br />
             <div>
                 <label htmlFor="birth_date">Date of Birth: </label>
-                <input type="date" id="birth_date" name="birth_date" value={newUserInfo.birth_date} />
-            </div><br />
+                    <input 
+                        type="date" 
+                        id="birth_date" 
+                        name="birth_date" 
+                        value={newUserInfo.birth_date}>
+                    </input>
+            </div>
+                <br />
             <div>
                 <label htmlFor="email">Email: </label>
-                <input type="email" id="email" name="email" value={newUserInfo.email} />
-            </div><br />
-            
-                <input type="submit" value="Login" />
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        value={newUserInfo.email}>
+                    </input>
+            </div>
+                <br />
+                <input id='formButton' type="submit" value="Create Account" />
           </form>
         </div>
     )
