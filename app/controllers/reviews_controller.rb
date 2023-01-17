60d5b4fa1ffe8ca12079f45a04dba96b276bca04
Michael <michael.daniels1013@gmail.com>
2023-01-17 10:25:03 -0600

    Reviews for specific user show in controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,18 @@
 class ReviewsController < ApplicationController
 
+    # def index
+    #     reviews = Review.all.order(rating: :asc)
+    #     render json: reviews, include: [:wine, :user]
+    # end
+
     def index
-        reviews = Review.all.order(rating: :asc)
-        render json: reviews, include: [:wine, :user]
+        reviews = current_user.reviews
+        render json: reviews
+    end
+
+    private
+
+    def current_user
+        User.find_by(id: session[:user_id])
     end
 end
