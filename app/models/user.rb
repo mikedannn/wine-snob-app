ba71224173ba451f5e33b708e8510efd2ef1fbe6
Michael <michael.daniels1013@gmail.com>
2023-01-27 14:48:12 -0600

    Login and Sign up working as they should:

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
 
     has_many :reviews, dependent: :destroy
     has_many :wines, dependent: :destroy
-    # has_many :wines, through: :reviews
+    has_many :wines, through: :reviews
 
 end
